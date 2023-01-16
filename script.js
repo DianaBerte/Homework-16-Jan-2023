72a2312ba69702d9f893def01015b5c5f109f65e
DianaBerte <dianaberte.go@gmail.com>
2023-01-16 17:15:01 +0100

    commit

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
 const getLists = () => {
-    fetch('https://deezerdevs-deezer.p.rapidapi.com/search?q=pinkfloyd', {
+    fetch('https://striveschool-api.herokuapp.com/api/deezer/search?q=pinkfloyd', {
         method: "GET",
         headers: {
             'X-RapidAPI-Key': '0cbcd173dfmshe0d3767927ec133p151910jsn1f811931eda4',
 		    'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
         }
-    }).then((rawLists)=> {
-        return rawLists.json()
-    }).then((jsonLists) => {
-        console.log(jsonLists.lists)
+    }).then((response)=> {
+        return response.json()
+    }).then((data) => {
+        console.log(data)
 
-        let ul = document.querySelector("ul");
+        let albums = data.data
 
-        for (let i = 0; i < jsonLists.length; i++) {
-             let singleList = jsonLists[i];
-             let li = document.createElement("li");
-             li.innerText = singleList.title
-             ul.appendChild(li)
-        } 
-    }).catch(err => console.error(err))
+
+
+      
+    })
+    
+    .catch(err => console.error(err))
+}
+
+const renderAlbums = (albums) => {
+    let rowNode = document.getElementsByClassName("row")[0];
+
+    for (let i = 0; i < albums.length; i++) {
+        rowNode.innerHTML += `
+        <div class="card">
+        <img src="..." class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">Card title</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+        </div>
+        `
+    }
 }
 
 getLists()
