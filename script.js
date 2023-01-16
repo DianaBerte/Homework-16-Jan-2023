65df97987a43fdbb4e82587702d9ad506859660f
DianaBerte <dianaberte.go@gmail.com>
2023-01-16 16:16:43 +0100

    commit

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-// fetch("https://deezerdevs-deezer.p.rapidapi.com/search?q=pink%20floyd")
-//     .then(rawSongs => rawSongs.json())
-//     .then(songs => {
-//         console.log(songs)
-//     })
-//     .catch(err => console.err(err))
-
 const getLists = () => {
-    fetch('https://striveschool-api.herokuapp.com/api/deezer/search?q=pinkfloyd', {
+    fetch('https://deezerdevs-deezer.p.rapidapi.com/search?q=pinkfloyd', {
         method: "GET",
         headers: {
             'X-RapidAPI-Key': '0cbcd173dfmshe0d3767927ec133p151910jsn1f811931eda4',
@@ -15,8 +8,17 @@ const getLists = () => {
     }).then((rawLists)=> {
         return rawLists.json()
     }).then((jsonLists) => {
-        console.log(jsonLists)
-    })
+        console.log(jsonLists.lists)
+
+        let ul = document.querySelector("ul");
+
+        for (let i = 0; i < jsonLists.length; i++) {
+             let singleList = jsonLists[i];
+             let li = document.createElement("li");
+             li.innerText = singleList.title
+             ul.appendChild(li)
+        } 
+    }).catch(err => console.error(err))
 }
 
 getLists()
